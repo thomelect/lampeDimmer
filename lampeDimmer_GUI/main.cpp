c949745352fb87a42c41923947c8f7dd6209460a
Thomas Desrosiers <thomas.desrosiers@live.fr>
2022-01-22 15:54:32 -0500

    + Ajout d'une fonction qui redirige les qDebugs, qinfos, etc vars un fichier de log.

--- lampeDimmer_GUI/main.cpp
+++ lampeDimmer_GUI/main.cpp
@@ -6,12 +6,47 @@
 */
 
 #include "mainwindow.h"
-
+#include <QFile>
+#include <QTextStream>
 #include <QApplication>
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QString txt;
+    switch (type)
+    {
+    case QtDebugMsg:
+        txt = QString("Debug: %1").arg(msg);
+        break;
+    case QtWarningMsg:
+        txt = QString("Warning: %1").arg(msg);
+        break;
+    case QtCriticalMsg:
+        txt = QString("Critical: %1").arg(msg);
+        break;
+    case QtFatalMsg:
+        txt = QString("Fatal: %1").arg(msg);
+        abort();
+    case QtInfoMsg:
+        txt = QString("Info: %1").arg(msg);
+        break;
+    }
+    QString fileName = "log";
+    QString formattedTime = QTime::currentTime().toString("hh:mm:ss");
+    QString formattedDate = QDate::currentDate().toString("yyyy-mm-dd");
+    QFile file(fileName);
+    file.setFileName(fileName);
+    file.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream out(&file);
+    out.setCodec("UTF-8");
+
+    out << txt << " -> " << formattedDate << formattedTime << endl; //Affichage de la date et de l'heure lors de la dernière écriture.
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    //qInstallMessageHandler(myMessageOutput);
     MainWindow w;
     w.show();
     return a.exec();
