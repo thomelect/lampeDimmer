e7304ce6f6a4c225696ef0f98a4a893349c263dd
thomelect <thomas.desrosiers@live.fr>
2022-12-23 16:50:57 -0500

    # [PRÉFÉRENCES] - Suppression d'options inutiles dans le menu de préférences.
    *[FONCTIONNEL]*

--- lampeDimmer_GUI/setuppreferencedialog.cpp
+++ lampeDimmer_GUI/setuppreferencedialog.cpp
@@ -8,10 +8,6 @@ SetupPreferenceDialog::SetupPreferenceDialog(QDialog *parent) : QDialog(parent),
     needReboot = 0;
     ui->setupUi(this);
 
-    ui->cbList_1->addItem("OUI");
-    ui->cbList_1->addItem("NON");
-    ui->cbList_1->addItem("PEUT-ÊTRE");
-
     QFrame *line = new QFrame();
     line->setFrameShape(QFrame::HLine);
     line->setFrameShadow(QFrame::Sunken);
@@ -26,9 +22,7 @@ SetupPreferenceDialog::SetupPreferenceDialog(QDialog *parent) : QDialog(parent),
     ui->cbOption_1->setChecked(settingsPref->value("option_1").toBool());
     ui->cbOption_2->setChecked(settingsPref->value("option_2").toBool());
     ui->cbOption_3->setChecked(settingsPref->value("option_3").toBool());
-    ui->cbOption_4->setChecked(settingsPref->value("option_4").toBool());
 
-    boutonManage();
 }
 
 SetupPreferenceDialog::~SetupPreferenceDialog()
@@ -36,22 +30,7 @@ SetupPreferenceDialog::~SetupPreferenceDialog()
     delete ui;
 }
 
-void SetupPreferenceDialog::boutonManage(void)
-{
-
-    if (ui->cbOption_4->isChecked())
-    {
-        ui->cbList_1->setEnabled(1);
-        ui->cbList_1->setCurrentIndex(settingsPref->value("optionMulti_2").toInt());
-    }
-    else
-    {
-        ui->cbList_1->setEnabled(0);
-        ui->cbList_1->setCurrentIndex(ui->cbList_1->findText("NON", Qt::MatchExactly));
-    }
-}
-
-void SetupPreferenceDialog::setAppToStartAutomatically(bool startAutomatically)
+void SetupPreferenceDialog::setDemarrageAuto(bool demarrageAuto)
 {
     QString key = "Lampe Dimmer";
 
@@ -59,7 +38,7 @@ void SetupPreferenceDialog::setAppToStartAutomatically(bool startAutomatically)
 
     registrySettings.remove(key);
 
-    if (startAutomatically)
+    if (demarrageAuto)
     {
         registrySettings.setValue(key, QString("\"" + windowsAppPath() + "\""));
     }
@@ -84,8 +63,6 @@ void SetupPreferenceDialog::on_btEnregistre_clicked()
     settingsPref->setValue("option_1", ui->cbOption_1->isChecked());
     settingsPref->setValue("option_2", ui->cbOption_2->isChecked());
     settingsPref->setValue("option_3", ui->cbOption_3->isChecked());
-    settingsPref->setValue("option_4", ui->cbOption_4->isChecked());
-    settingsPref->setValue("optionMulti_1", ui->cbList_1->currentIndex());
 
     if (!needReboot)
     {
@@ -95,13 +72,9 @@ void SetupPreferenceDialog::on_btEnregistre_clicked()
     {
         ui->pbReboot->setVisible(needReboot);
     }
-    setAppToStartAutomatically(ui->cbOption_3->isChecked());
+    setDemarrageAuto(ui->cbOption_3->isChecked());
 }
 
-void SetupPreferenceDialog::on_cbOption_4_clicked()
-{
-    boutonManage();
-}
 
 void SetupPreferenceDialog::on_pbReboot_clicked()
 {
